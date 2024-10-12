beb766e8e8669422080113a9892130054149c263
alex-vesel <avesel@stanford.edu>
2024-10-12 16:11:31 -0700

    add init user graph to server

--- src/backend/server.py
+++ src/backend/server.py
@@ -1,37 +1,42 @@
 from flask import Flask, request
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 import requests
 from flask import jsonify
 import json
 from dotenv import load_dotenv
 import openai
 import os
-from x_api import XAPI
+from orchestrator import Orchestrator
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+# pip install python-dotenv
 
+CORS(app) 
 dir = os.path.dirname(__file__)
 load_dotenv(dotenv_path=os.path.join(dir, '../secrets.env'))
 
 tweet_id_to_url = lambda tweet_id: f"https://twitter.com/twitter/status/{tweet_id}"
 
 # Global vars
-x_api = XAPI()
+orchestrator = Orchestrator()
 
 def jsonify_tweet_list(tweets):
-    if not tweets:
-        tweets = []
-    print(f"{tweets}")
     return jsonify([{"id": tweet.id, "text": tweet.text, "url": tweet_id_to_url(tweet.id)} for tweet in tweets])
 
 
 @app.route('/tweets_from_category')
 def tweets_from_category():
-    tweets = x_api.get_tweets_from_category(request.args.get('category'))
+    tweets = orchestrator.x_api.get_tweets_from_category(request.args.get('category'))
     return jsonify_tweet_list(tweets)
+
+
+@app.route('/init_user_graph')
+def init_user_graph():
+    graph = orchestrator.init_user_graph(request.args.get('username'))
+    graph_json = graph.to_server_format()
+    return jsonify(graph_json)
 
 
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run()
